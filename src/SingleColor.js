7e94cc43660a24e54c09b51a314272d58d80aca3
Sourav <s6hr6h4u3@gmail.com>
2022-03-25 15:49:16 +0530

    initial commit

---
+++ src/SingleColor.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const SingleColor = ({ weight, type, hex }) => {
+    const [alert, setAlert] = useState("");
+    const hexVal = `#${hex}`;
+    const handleColorClick = (val) => {
+        setAlert(val);
+        navigator.clipboard.writeText(val);
+        setTimeout(() => {
+            setAlert("");
+        }, 2500);
+    };
+    return (
+        <>
+            <article
+                onClick={() => handleColorClick("#" + hex)}
+                className={`color ${type === "shade" ? "color-light" : ""}`}
+                style={{ background: hexVal }}>
+                <p className="percent-value">{weight}%</p>
+                <p className="color-value">{hexVal}</p>
+                {alert === hexVal ? <p className="alert">copied</p> : <></>}
+            </article>
+        </>
+    );
+};
+
+export default SingleColor;
